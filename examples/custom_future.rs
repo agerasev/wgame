agerasev/event-horizon

WIP: Timers and Runtime, examples dir

examples/custom_future.rs
@@ -6,7 +6,7 @@ use std::{
     time::Duration,
 };
 
-use event_horizon::{executor::enter, window::Window};
+use event_horizon::{executor::enter, runtime::Runtime};
 
 #[derive(Default)]
 struct LoaderInfo {
@@ -49,14 +49,10 @@ impl Future for Loader {
     }
 }
 
-async fn main_(mut window: Window) {
-    println!("Started");
+async fn main_(_rt: Runtime) {
+    println!("Going to sleep");
     Loader::new(Duration::from_secs(1)).await;
-    println!("Loaded");
-    while let Some(_render) = window.request_render().await {
-        println!("Rendered");
-    }
-    println!("Closed");
+    println!("Awakened");
 }
 
 fn main() {

examples/render_loop.rs
@@ -1,0 +1,13 @@
+use event_horizon::{executor::enter, runtime::Runtime};
+
+async fn main_(rt: Runtime) {
+    println!("Started");
+    while let Some(_render) = rt.request_render().await {
+        println!("Rendered");
+    }
+    println!("Closed");
+}
+
+fn main() {
+    enter(main_);
+}

examples/sleep.rs
@@ -1,0 +1,13 @@
+use std::time::Duration;
+
+use event_horizon::{executor::enter, runtime::Runtime};
+
+async fn main_(rt: Runtime) {
+    println!("Going to sleep");
+    rt.sleep(Duration::from_secs(1)).await;
+    println!("Awakened");
+}
+
+fn main() {
+    enter(main_);
+}

agerasev/wgame

examples/custom_future.rs
@@ -6,7 +6,7 @@ use std::{
     time::Duration,
 };
 
-use wgame::{executor::enter, window::Window};
+use wgame::{executor::enter, runtime::Runtime};
 
 #[derive(Default)]
 struct LoaderInfo {
@@ -49,14 +49,10 @@ impl Future for Loader {
     }
 }
 
-async fn main_(mut window: Window) {
-    println!("Started");
+async fn main_(_rt: Runtime) {
+    println!("Going to sleep");
     Loader::new(Duration::from_secs(1)).await;
-    println!("Loaded");
-    while let Some(_render) = window.request_render().await {
-        println!("Rendered");
-    }
-    println!("Closed");
+    println!("Awakened");
 }
 
 fn main() {

examples/render_loop.rs
@@ -1,0 +1,13 @@
+use wgame::{executor::enter, runtime::Runtime};
+
+async fn main_(rt: Runtime) {
+    println!("Started");
+    while let Some(_render) = rt.request_render().await {
+        println!("Rendered");
+    }
+    println!("Closed");
+}
+
+fn main() {
+    enter(main_);
+}

examples/sleep.rs
@@ -1,0 +1,13 @@
+use std::time::Duration;
+
+use wgame::{executor::enter, runtime::Runtime};
+
+async fn main_(rt: Runtime) {
+    println!("Going to sleep");
+    rt.sleep(Duration::from_secs(1)).await;
+    println!("Awakened");
+}
+
+fn main() {
+    enter(main_);
+}