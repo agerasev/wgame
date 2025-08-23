deb663d1a77dfa11cb43525627cc97653642eed9
Alexey Gerasev <alexey.gerasev@gmail.com>
2025-08-23 17:01:57 +0700

    Convert image to sRGB while loading

--- wgame-img/src/lib.rs
+++ wgame-img/src/lib.rs
@@ -12,13 +12,17 @@ pub fn image_to_texture(state: &Library, bytes: &[u8]) -> Result<Texture> {
     let reader = Cursor::new(bytes);
     let image = ImageReader::new(reader).with_guessed_format()?.decode()?;
 
-    // TODO: Convert directly to f16
-    let data: Vec<f16> = image
-        .to_rgba32f()
-        .into_vec()
-        .into_iter()
-        .map(f16::from_f32)
-        .collect();
+    let data: Vec<f16> = {
+        // TODO: Convert directly to f16
+        let mut image = image.to_rgba32f();
+        // Convert to sRGB
+        for pix in image.pixels_mut() {
+            for ch in &mut pix.0[0..3] {
+                *ch = ch.powf(2.2);
+            }
+        }
+        image.into_vec().into_iter().map(f16::from_f32).collect()
+    };
 
     Ok(state.texture_with_data((image.width(), image.height()), bytemuck::cast_slice(&data)))
 }
