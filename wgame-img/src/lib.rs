90911771642eae90eb79ecd69006bac36b365313
Alexey Gerasev <alexey.gerasev@gmail.com>
2025-09-12 08:56:05 +0700

    Image implementation

--- wgame-img/src/lib.rs
+++ wgame-img/src/lib.rs
@@ -1,12 +1,11 @@
 #![forbid(unsafe_code)]
 
 use std::{
-    borrow::Cow,
     io::Cursor,
     ops::{Bound, Range, RangeBounds},
 };
 
-use anyhow::{Result, bail};
+use anyhow::Result;
 use bytemuck::Pod;
 use half::f16;
 use image::ImageReader;
@@ -19,8 +18,12 @@ impl Pixel for Rgba<f16> {}
 
 pub trait ImageLike<T: Pixel> {
     fn size(&self) -> (u32, u32);
-    fn stride(&self) -> (u32, u32);
+    fn stride(&self) -> u32;
     fn data(&self) -> &[T];
+}
+
+pub trait ImageLikeMut<T: Pixel>: ImageLike<T> {
+    fn data_mut(&mut self) -> &mut [T];
 }
 
 pub struct Image<T: Pixel = Rgba<f16>> {
@@ -30,8 +33,14 @@ pub struct Image<T: Pixel = Rgba<f16>> {
 
 pub struct ImageSlice<'a, T: Pixel> {
     size: (u32, u32),
-    stride: (u32, u32),
+    stride: u32,
     data: &'a [T],
+}
+
+pub struct ImageSliceMut<'a, T: Pixel> {
+    size: (u32, u32),
+    stride: u32,
+    data: &'a mut [T],
 }
 
 impl<T: Pixel> Image<T> {
@@ -55,11 +64,47 @@ impl<T: Pixel> ImageLike<T> for Image<T> {
     fn size(&self) -> (u32, u32) {
         self.size
     }
-    fn stride(&self) -> (u32, u32) {
-        (1, self.size.0)
+    fn stride(&self) -> u32 {
+        self.size.0
     }
     fn data(&self) -> &[T] {
         &self.data
+    }
+}
+
+impl<T: Pixel> ImageLikeMut<T> for Image<T> {
+    fn data_mut(&mut self) -> &mut [T] {
+        &mut self.data
+    }
+}
+
+impl<T: Pixel> ImageLike<T> for ImageSlice<'_, T> {
+    fn size(&self) -> (u32, u32) {
+        self.size
+    }
+    fn stride(&self) -> u32 {
+        self.stride
+    }
+    fn data(&self) -> &[T] {
+        self.data
+    }
+}
+
+impl<T: Pixel> ImageLike<T> for ImageSliceMut<'_, T> {
+    fn size(&self) -> (u32, u32) {
+        self.size
+    }
+    fn stride(&self) -> u32 {
+        self.stride
+    }
+    fn data(&self) -> &[T] {
+        self.data
+    }
+}
+
+impl<T: Pixel> ImageLikeMut<T> for ImageSliceMut<'_, T> {
+    fn data_mut(&mut self) -> &mut [T] {
+        self.data
     }
 }
 
@@ -113,15 +158,95 @@ pub trait ImageLikeExt<T: Pixel>: ImageLike<T> {
         let size = self.size();
         let x_range = into_range(x_range, size.0);
         let y_range = into_range(y_range, size.1);
-
-        let new_size = (x_range.end - x_range.start, y_range.end - y_range.start);
-        let stride = self.stride();
+        let data_start = x_range.start as usize + self.stride() as usize * y_range.start as usize;
+        let data_end = x_range.end as usize + self.stride() as usize * y_range.end as usize;
         ImageSlice {
-            size: new_size,
-            stride: (stride.0, stride.1 + (size.0 - new_size.0) * stride.0),
-            data: &self.data()[unimplemented!()],
+            size: (x_range.end - x_range.start, y_range.end - y_range.start),
+            stride: self.stride(),
+            data: &self.data()[data_start..data_end],
+        }
+    }
+
+    fn get(&self, i: u32, j: u32) -> &T {
+        let size = self.size();
+        let stride = self.stride();
+        assert!(
+            i < size.0 && j < size.1,
+            "({i}, {j}) is out of bounds {size:?}"
+        );
+        &self.data()[i as usize + stride as usize * j as usize]
+    }
+
+    fn rows(&self) -> impl ExactSizeIterator<Item = (u32, &[T])> {
+        let size = self.size();
+        let stride = self.stride();
+        self.data()
+            .chunks(stride as usize)
+            .enumerate()
+            .map(move |(j, row)| (j as u32, row.split_at(size.0 as usize).0))
+    }
+
+    fn pixels(&self) -> impl Iterator<Item = ((u32, u32), &T)> {
+        self.rows().flat_map(|(j, row)| {
+            row.iter()
+                .enumerate()
+                .map(move |(i, pixel)| ((i as u32, j), pixel))
+        })
+    }
+}
+
+pub trait ImageLikeMutExt<T: Pixel>: ImageLikeMut<T> {
+    fn slice_mut(
+        &mut self,
+        x_range: impl RangeBounds<u32>,
+        y_range: impl RangeBounds<u32>,
+    ) -> ImageSliceMut<'_, T> {
+        let size = self.size();
+        let x_range = into_range(x_range, size.0);
+        let y_range = into_range(y_range, size.1);
+        let data_start = x_range.start as usize + self.stride() as usize * y_range.start as usize;
+        let data_end = x_range.end as usize + self.stride() as usize * y_range.end as usize;
+        ImageSliceMut {
+            size: (x_range.end - x_range.start, y_range.end - y_range.start),
+            stride: self.stride(),
+            data: &mut self.data_mut()[data_start..data_end],
+        }
+    }
+
+    fn get_mut(&mut self, i: u32, j: u32) -> &mut T {
+        let size = self.size();
+        let stride = self.stride();
+        assert!(
+            i < size.0 && j < size.1,
+            "({i}, {j}) is out of bounds {size:?}"
+        );
+        &mut self.data_mut()[i as usize + stride as usize * j as usize]
+    }
+
+    fn rows_mut(&mut self) -> impl ExactSizeIterator<Item = (u32, &mut [T])> {
+        let size = self.size();
+        let stride = self.stride();
+        self.data_mut()
+            .chunks_mut(stride as usize)
+            .enumerate()
+            .map(move |(j, row)| (j as u32, row.split_at_mut(size.0 as usize).0))
+    }
+
+    fn pixels_mut(&mut self) -> impl Iterator<Item = ((u32, u32), &mut T)> {
+        self.rows_mut().flat_map(|(j, row)| {
+            row.iter_mut()
+                .enumerate()
+                .map(move |(i, pixel)| ((i as u32, j), pixel))
+        })
+    }
+
+    fn copy_from(&mut self, src: impl ImageLike<T>) {
+        assert_eq!(self.size(), src.size());
+        for ((_, dst), (_, src)) in self.rows_mut().zip(src.rows()) {
+            dst.copy_from_slice(src);
         }
     }
 }
 
 impl<T: Pixel, I: ImageLike<T>> ImageLikeExt<T> for I {}
+impl<T: Pixel, I: ImageLikeMut<T>> ImageLikeMutExt<T> for I {}
